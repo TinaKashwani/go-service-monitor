c331f25dbb3d2fa1821e745372534307ad184040
Tina Kashwani <90719959+TinaKashwani@users.noreply.github.com>
2026-08-11 16:26:22 -0300

    Add monitor analytics and incident views

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,4 +2,6 @@ import { Routes } from '@angular/router';
 import { PlaceholderComponent } from './pages/placeholder.component';
 import { OverviewComponent } from './pages/overview.component';
 import { MonitorsComponent } from './pages/monitors.component';
-export const routes:Routes=[{path:'overview',component:OverviewComponent},{path:'monitors',component:MonitorsComponent},{path:'monitors/:id',component:PlaceholderComponent},{path:'incidents',component:PlaceholderComponent},{path:'about',component:PlaceholderComponent},{path:'',pathMatch:'full',redirectTo:'overview'},{path:'**',redirectTo:'overview'}];
+import { MonitorDetailComponent } from './pages/monitor-detail.component';
+import { IncidentsComponent } from './pages/incidents.component';
+export const routes:Routes=[{path:'overview',component:OverviewComponent},{path:'monitors',component:MonitorsComponent},{path:'monitors/:id',component:MonitorDetailComponent},{path:'incidents',component:IncidentsComponent},{path:'about',component:PlaceholderComponent},{path:'',pathMatch:'full',redirectTo:'overview'},{path:'**',redirectTo:'overview'}];
